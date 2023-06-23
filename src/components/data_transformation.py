cb8d5f2422a9f1b54fb8f4e633ba2ff99f67fcbf
Irtaza Ahmed <kaziirtaza@gmail.com>
2023-06-24 01:20:42 +0500

    final commit

---
+++ src/components/data_transformation.py
@@ -0,0 +1,150 @@
+import sys
+import os
+from dataclasses import dataclass
+import numpy as np 
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, RobustScaler, OneHotEncoder
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object
+from imblearn.over_sampling import SMOTE
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
+    trainos_data_path: str=os.path.join('artifacts',"train_os.csv")
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+
+        try:
+            outliers = ["hypertension", "heart_disease", "bmi", "HbA1c_level", "blood_glucose_level"]
+
+            cat = ['gender', 'smoking_history']
+            
+            no_outliers = ['age']
+            
+            outliers_pipeline= Pipeline( steps=
+                                        [
+                                         ("rs", RobustScaler())])
+            
+            no_outliers_pipeline = Pipeline( steps=
+                                        [
+                                         ("ss", StandardScaler())])
+
+            cat_pipeline = Pipeline( steps=
+                                  [
+                                   ('ohe', OneHotEncoder())])
+            
+            preprocessor = ColumnTransformer(
+                [
+                    ("outliers_pipeline", outliers_pipeline, outliers),
+                    ("no_outliers_pipeline", no_outliers_pipeline, no_outliers),
+                    ("cat_pipeline", cat_pipeline, cat)
+                ]
+            )
+
+
+
+            return preprocessor
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    def initiate_data_transformation(self,train_path,test_path):
+
+        try:
+            train = pd.read_csv(train_path)
+
+            logging.info("Read train data")
+            
+            test = pd.read_csv(test_path)
+
+            logging.info("Read test data")
+
+            os.makedirs(os.path.dirname(self.data_transformation_config.trainos_data_path),exist_ok=True)
+
+            logging.info ("directory made for df_os")
+
+            x_train_transf = train.drop('diabetes',axis=1)
+
+            logging.info("Dropped target column from the train set to make the input data frame for model training")
+
+            y_train_transf = train['diabetes']
+
+            logging.info("Target feature obtained for model training")
+
+            x_test_transf = test.drop('diabetes', axis=1)
+
+            logging.info("Dropped target column from the test set to make the input data frame for model testing")
+        
+            y_test_transf = test['diabetes']
+
+            logging.info("Target feature obtained for model testing")
+
+            preprocessor = self.get_data_transformer_object()
+            
+            logging.info("Preprocessing object obtained")
+
+            x_train_transf_preprocessed = preprocessor.fit_transform(x_train_transf)
+
+            logging.info("Preprocessor applied on x_train_transf")
+
+            x_train_transf_preprocessed_df = pd.DataFrame(x_train_transf_preprocessed)
+
+            logging.info('''x_train_transf dataframe formed for feature selection by VIF''')
+            
+            for i in range(len(x_train_transf_preprocessed_df.columns)):
+                
+                x_train_transf_preprocessed_df = x_train_transf_preprocessed_df.rename(columns={x_train_transf_preprocessed_df.columns[i]: f'c{i+1}'})
+
+            logging.info('''x_train_transf dataframe columns renamed''')
+            
+            over_samp = SMOTE(k_neighbors=1)
+            
+            logging.info ("oversampling initiated")
+            
+            x_train_os, y_train_os = over_samp.fit_resample (x_train_transf_preprocessed_df, y_train_transf)
+
+            logging.info ("oversampling completed")
+
+            x_test_transf_preprocessed = preprocessor.transform(x_test_transf)
+
+            logging.info("Preprocessor applied on x_test_transf")
+
+            x_test_transf_preprocessed_df = pd.DataFrame(x_test_transf_preprocessed)
+
+            logging.info('''x_test_transf dataframe formed for feature selection vif''')
+            
+            for i in range(len(x_test_transf_preprocessed_df.columns)):
+                
+                x_test_transf_preprocessed_df = x_test_transf_preprocessed_df.rename(columns={x_test_transf_preprocessed_df.columns[i]: f'c{i+1}'})
+
+            logging.info('''x_test_transf dataframe columns renamed''')
+            
+            train_arr = np.c_[np.array(x_train_os), np.array(y_train_os)]
+            
+            logging.info("Combined the input features and target feature of the train set as an array.")
+            
+            test_arr = np.c_[np.array(x_test_transf_preprocessed_df), np.array(y_test_transf)]
+            
+            logging.info("Combined the input features and target feature of the test set as an array.")
+            
+            save_object(
+            file_path=self.data_transformation_config.preprocessor_obj_file_path,
+            obj=preprocessor)
+            
+            logging.info("Saved preprocessing object.")
+            
+            return (
+            train_arr,
+            test_arr,
+            self.data_transformation_config.preprocessor_obj_file_path,)
+        
+        except Exception as e:
+            raise CustomException(e, sys)
